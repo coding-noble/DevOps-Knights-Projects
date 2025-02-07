c4979921e1648b7162f6dfa2a227682651fe9bbc
Miles Noble <105387328+coding-noble@users.noreply.github.com>
2025-02-07 15:27:53 -0700

    Create Database

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import styles from './page.module.css'; 
+import styles from './page.module.css';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <p>© 2025 DevOps Knights. All rights reserved.</p>
       <ul className={styles.socialLinks}>
-        <li><a href="https://twitter.com">Twitter</a></li>
-        <li><a href="https://github.com">GitHub</a></li>
-        <li><a href="https://linkedin.com">LinkedIn</a></li>
+        <li><Link href="https://twitter.com">Twitter</Link></li>
+        <li><Link href="https://github.com">GitHub</Link></li>
+        <li><Link href="https://linkedin.com">LinkedIn</Link></li>
       </ul>
     </footer>
   );
--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import styles from './page.module.css'; 
+import styles from './page.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        <img src="/next.svg" alt="Logo" />
+        <Image src="/logo.png" alt="Logo" width={100} height={50} />
         <h1>DevOps Knights</h1>
       </div>
       <nav>
         <ul className={styles.navList}>
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/about">About</Link></li>
+          <li><Link href="/contact">Contact</Link></li>
         </ul>
       </nav>
     </header>
